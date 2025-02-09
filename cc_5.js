566ce357753e83170a82e77b3d186cd74e1fd041
Abdulhakim <Abdulthe383@gmail.com>
2025-02-08 23:57:29 -0500

    Task 2: Object Methods

--- cc_5.js
+++ cc_5.js
@@ -6,6 +6,14 @@ email: "john.doe@email.com", // email
 };
 console.log("Name: " + customer.name + ", Age: " + customer.age + ", Email: " + customer.email); // logged
 
-
+// Task 2 - Order Details //
+let order = {
+orderId: 12345, // order id
+totalAmount: 150, // Total Amount
+status: "Processing", // status
+displayOrder: function() 
+{ console.log("Order ID: " + this.orderId + ", Total: $" + this.totalAmount + ", Status: " + this.status);}}; // logged
+order.displayOrder();
+    
     
     
