a35da7159291622e2642441a1c9896f86b4da850
Abdulhakim <Abdulthe383@gmail.com>
2025-02-08 23:55:33 -0500

    Task 1: Object Properties

---
+++ cc_5.js
@@ -0,0 +1,11 @@
+// Task 1 - Customer Profile //
+let customer = {
+name: "John Doe", // name
+age: 35, //age 
+email: "john.doe@email.com", // email
+};
+console.log("Name: " + customer.name + ", Age: " + customer.age + ", Email: " + customer.email); // logged
+
+
+    
+    
